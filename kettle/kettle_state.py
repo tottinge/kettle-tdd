d86b7b7775698a86d84f696ebea4d7f3beb348b3
Tim Ottinger <tottinge@industriallogic.com>
2023-05-26 09:27:58 +0200

    refactor: to state. 1) changed KettleState to not be enum 2) Created ABC for State, with default implementations for enter/exit 3) Added a NullState (null object pattern) so we can exit it. 4) changed enter_state to be delegate (no if/else, match, etc) 5) Made device control methods non-private 6) Moved if/else logic from 'on_' methods to state classes.

--- kettle/kettle_state.py
+++ kettle/kettle_state.py
@@ -1,6 +1,47 @@
-from enum import Enum
+from abc import ABC
 
 
-class KettleState(Enum):
-    IDLE = 0
-    HEATING = 1
+class State(ABC):
+    def enter(self, kettle):
+        ...
+
+    def exit(self, kettle):
+        ...
+
+    def on_button_press(self, kettle):
+        return self
+
+    def on_temp_reached_or_exceeded(self, kettle):
+        return self
+
+    def on_pot_lifted(self, kettle):
+        return self
+
+
+class Heating(State):
+    def enter(self, kettle):
+        kettle.start_heater()
+        kettle.light_on()
+
+    def exit(self, kettle):
+        kettle.stop_heater()
+        kettle.light_off()
+
+    def on_temp_reached_or_exceeded(self, kettle):
+        return KettleState.IDLE
+
+    def on_pot_lifted(self, kettle):
+        return KettleState.IDLE
+
+
+class Idle(State):
+
+    def enter(self, kettle):
+        kettle.stop_heater()
+        kettle.light_off()
+
+
+class KettleState:
+    NullState = State()
+    IDLE = Idle()
+    HEATING = Heating()
--- kettle/mykettle.py
+++ kettle/mykettle.py
@@ -1,32 +1,21 @@
 from kettle.heater import Heater
-from kettle.kettle_state import KettleState
+from kettle.kettle_state import KettleState, State
 from kettle.light import Light
 
 
 class Kettle:
-    _state: KettleState
+    _state: State = KettleState.NullState
     _heater: Heater
     _light: Light
 
-    def __init__(self, initial_state: KettleState = KettleState.IDLE):
+    def __init__(self, initial_state: State = KettleState.IDLE):
         self._heater = Heater()
         self._light = Light()
-        self.enter_state(initial_state)
+        self._enter_state(initial_state)
 
-    def enter_state(self, state: KettleState):
-        match state:
-            case KettleState.IDLE:
-                self._stop_heater()
-                self._light_off()
-            case KettleState.HEATING:
-                self._start_heater()
-                self._light_on()
-            case _:
-                 # unhandled cases?
-                 # what do we do if we add a state
-                 # and forget to handle it in the
-                 # new state?
-                ...
+    def _enter_state(self, state: State):
+        self._state.exit(self)
+        state.enter(self)
         self._state = state
 
     def is_light_on(self) -> bool:
@@ -36,31 +25,31 @@ class Kettle:
         return self._heater.is_heating()
 
     def on_button_press(self) -> None:
-        if self._state is KettleState.IDLE:
-            ...
-            # self.enter_state(KettleState.HEATING)
-        else:
-            pass
+        self._enter_state(
+            self._state.on_button_press(self)
+        )
 
     def on_temp_reached_or_exceeded(self) -> None:
-        if self._state is KettleState.HEATING:
-            self.enter_state(KettleState.IDLE)
+        self._enter_state(
+            self._state.on_temp_reached_or_exceeded(self)
+        )
 
     def on_pot_lifted(self) -> None:
-        if self._state is KettleState.HEATING:
-            self.enter_state(KettleState.IDLE)
+        self._enter_state(
+            self._state.on_pot_lifted(self)
+        )
 
-    def _light_on(self):
+    def light_on(self):
         """
         This would be much more interesting in the real world.
         """
         self._light.turn_on()
 
-    def _light_off(self):
+    def light_off(self):
         self._light.turn_off()
 
-    def _start_heater(self):
+    def start_heater(self):
         self._heater.turn_on()
 
-    def _stop_heater(self):
+    def stop_heater(self):
         self._heater.turn_off()
